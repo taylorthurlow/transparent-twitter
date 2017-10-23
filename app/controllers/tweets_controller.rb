a8f097626fe2fe230160aa48d0e5bfd31793b1f4
Taylor Thurlow <taylorthurlow8@gmail.com>
2017-10-22 21:33:51 -0700

    Fixed twitter authentication

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -44,10 +44,10 @@ l  end
 
     def twitter_init
       return Twitter::REST::Client.new do |config|
-        config.consumer_key = Rails.application.secrets.twitter_consumer_key
-        config.consumer_secret = Rails.application.secrets.twitter_consumer_secret
-        config.access_token = Rails.application.secrets.twitter_access_token
-        config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+        config.consumer_key = ENV['SECRET_CONSUMER_KEY']
+        config.consumer_secret = ENV['SECRET_CONSUMER_SECRET']
+        config.access_token = ENV['SECRET_ACCESS_TOKEN']
+        config.access_token_secret = ENV['SECRET_ACCESS_TOKEN_SECRET']
       end
     end
 
